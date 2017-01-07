61ad3c696384a0201201da7fb30159edec16d7ea
Swati Gupta <swati.gu11@gmail.com>
2017-01-07 17:52:32 +0530

    Update TestVideoSpec.js

--- TestVideoSpec.js
+++ TestVideoSpec.js
@@ -23,3 +23,33 @@ describe("TestVideoSpec", function() {
 });
 
 
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Jasmine Spec Runner v2.5.2</title>
+
+  <link rel="shortcut icon" type="image/png" href="lib/jasmine-2.5.2/jasmine_favicon.png">
+  <link rel="stylesheet" href="lib/jasmine-2.5.2/jasmine.css">
+
+  <script src="lib/jasmine-2.5.2/jasmine.js"></script>
+  <script src="lib/jasmine-2.5.2/jasmine-html.js"></script>
+  <script src="lib/jasmine-2.5.2/boot.js"></script>
+  <script src="jquery.js"></script>
+  <script src="jasmine-jquery.js"></script>
+
+  <script src="https://content.jwplatform.com/libraries/URxg89az.js"></script>
+  <script>jwplayer.key="dN8rYM3cyRXw9+6UI9FEMnq1UQWgMa6Ld3sheQ==";</script>
+
+  <!-- include spec files here... -->
+  <script src="spec/TestVideoSpec.js"></script>
+
+</head>
+
+<body>
+   <script type="text/javascript" src="../js/main.js"></script>
+</body>
+</html>
+
+
+
